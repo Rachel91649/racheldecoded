071a815a78ccc3a3a895a4b19a72debb7a108ad9
Rachel91649 <rachelnjackson@yahoo.com>
2018-01-25 14:41:03 -0500

    started navingation layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
+//Dependency Imports
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+//File Imports
 import './index.css';
-import App from './App';
+//import '../public/css/layout.css'
+//import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => (
+  <div>
+    <div>
+      <nav className="nav nav-tabs justify-content-center">
+        <i className="fa fa-home nav" aria-hidden="true"><Link className="nav" to="/">Home</Link></i>
+        {"  "}
+        <i className="fa fa-user-circle-o nav" aria-hidden="true"><Link className="nav" to="/about">About Me</Link></i>
+        {"  "}
+        <i className="fa fa-magic nav" aria-hidden="true"><Link className="nav" to="/skills">Skills</Link></i>
+        {" "}
+        <i className="fa fa-code nav" aria-hidden="true"><Link className="nav" to="/projects">Projects</Link></i>
+      </nav>
+    </div>
+    <div>
+      
+    </div>
+  </div>
+);
+
+render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+	, document.getElementById('root'));
 registerServiceWorker();
